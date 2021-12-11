706c53e10c7f9784f75ac71e8d4c3f901065b738
Jonathan Glaser <49253432+jmg764@users.noreply.github.com>
2021-12-11 17:08:05 -0500

    Update model_def.py

--- code/model_def.py
+++ code/model_def.py
@@ -73,5 +73,19 @@ class Attention(nn.Module):
 
         return Y_prob, Y_hat, A.byte()
 
+  def calculate_classification_error(self, X, Y):
+      Y = Y.float()
+      _, Y_hat, _ = self.forward(X)
+      error = 1. - Y_hat.eq(Y).cpu().float().mean().data
+
+      return error, Y_hat
+
+  def calculate_objective(self, X, Y):
+      Y = Y.float()
+      Y_prob, _, A = self.forward(X)
+      Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+      neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))
+
+      return neg_log_likelihood, A
 
 
